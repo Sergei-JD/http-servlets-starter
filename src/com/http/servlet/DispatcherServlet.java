b4e70281eab2b87654802e824f65f8f80681b6f5
Sergei-JD <sergshlyazhko@gmail.com>
2022-01-22 20:00:47 +0300

    added JSP examples and examples for working with EL and JSTL

--- src/com/http/servlet/DispatcherServlet.java
+++ src/com/http/servlet/DispatcherServlet.java
@@ -14,6 +14,7 @@ public class DispatcherServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/flights")
+//                .forward(req,resp)
                 .include(req, resp);
         var writer = resp.getWriter();
         writer.write("all that is below when redirecting \"forward\" the user will not see, and when \"include\" - will see!!");

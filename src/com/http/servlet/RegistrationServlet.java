6e4bc347d9662b0d84411b626d1c855328b5c315
Sergei-JD <sergshlyazhko@gmail.com>
2022-01-23 13:54:52 +0300

    added filters

--- src/com/http/servlet/RegistrationServlet.java
+++ src/com/http/servlet/RegistrationServlet.java
@@ -15,13 +15,17 @@ import java.io.IOException;
 import java.util.List;
 
 @MultipartConfig(fileSizeThreshold = 1024 *1024)
-@WebServlet("/registration")
+@WebServlet(value = "/registration", name = "RegistrationServlet")
 public class RegistrationServlet extends HttpServlet {
 
     private final UserService userService = UserService.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        to display an error page instead of a stack trace
+//        if (true) {
+//            throw new RemoteException();
+//        }
         req.setAttribute("roles", List.of("USER", "ADMIN"));
         req.setAttribute("genders", List.of("MALE", "FEMALE"));
 
